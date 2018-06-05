7ce9316f1f78f4376ebdc1a0bc6d75c343eb81f5
Buhle Mwanza <buhle.mwanza@gmail.com>
2018-06-04 23:38:07 -0500

    Created link to create project page

--- app/src/main/java/Presentation/CreateProjectActivity.java
+++ app/src/main/java/Presentation/CreateProjectActivity.java
@@ -2,17 +2,28 @@ package Presentation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.test.ppms.R;
 
 public class CreateProjectActivity extends AppCompatActivity {
 
+    //For the UI
     private EditText mProjectNameEdit;
     private EditText mProjectDescriptionEdit;
     private Button mCreateProjectButton;
     private Button mCancelButton;
+
+
+    //For the new Project
+    private String mProjectName;
+    private String mProjectDescr;
 
 
     @Override
@@ -24,6 +35,42 @@ public class CreateProjectActivity extends AppCompatActivity {
         mProjectDescriptionEdit = (EditText) findViewById(R.id.project_description);
         mCreateProjectButton = (Button) findViewById(R.id.create_course_button);
         mCancelButton = (Button) findViewById(R.id.cancel_course_button);
+
+        mProjectNameEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+                boolean actionDone = false;
+                if(actionId == EditorInfo.IME_ACTION_DONE) {
+                    mProjectName = textView.getText().toString();
+                    actionDone = true;
+                }
+                return actionDone;
+            }
+        });
+
+        mCreateProjectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mProjectNameEdit.getText().toString().equals("") ||
+                        mProjectDescriptionEdit.getText().toString().equals("")) {
+                    Toast.makeText(CreateProjectActivity.this, R.string.enter_all_fields_prompt,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    mProjectName = mProjectNameEdit.getText().toString();
+                    mProjectDescr = mProjectDescriptionEdit.getText().toString();
+
+                    /**
+                     * TO-DO: Change this
+                     * For now just send the new Project info to be added to the DB
+                     */
+
+                    //Close the Acivity after the Project is created
+                    finish();
+                }
+            }
+        });
+
+
 
     }
 }
--- app/src/main/java/Presentation/MainActivity.java
+++ app/src/main/java/Presentation/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.test.ppms;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import Presentation.CreateProjectActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mLogInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         //______________//
         //Splash Screen
@@ -35,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLogInButton = (Button) findViewById(R.id.button3);
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateProjectActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
