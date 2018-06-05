2e6b75cb006d6c2da10abd06c1c3d22c9b32a685
Buhle Mwanza <BMwanza@Buhles-MacBook-Pro.local>
2018-06-05 18:20:36 -0500

    updated manifest file and fixed links between Main Actvity and Project Activity

--- app/src/main/java/Presentation/MainActivity.java
+++ app/src/main/java/Presentation/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.test.ppms;
+package Presentation;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.test.ppms.R;
 
 import Presentation.CreateProjectActivity;
 
@@ -43,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLogInButton = (Button) findViewById(R.id.button3);
+        mLogInButton = (Button) findViewById(R.id.button);
 
         mLogInButton.setOnClickListener(new View.OnClickListener() {
             @Override
