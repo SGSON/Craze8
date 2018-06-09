a698462c573ffd546d5ef9e456fbfdd92594664a
Wilson <zhangw22@myumanitoba.ca>
2018-06-09 16:09:45 -0500

    Changed ProjectManger Interface and ProjectManager to test my test cases. Currently what I have

--- app/src/main/java/logic/ProjectManager.java
+++ app/src/main/java/logic/ProjectManager.java
@@ -6,31 +6,47 @@ import domain.DatabaseObject;
  * Here is the Project Manager class that will handle
  * and validate data of a Project object. 
  */
-public class ProjectManager implements ProjectManagerInterface{
+//public class ProjectManager implements ProjectManagerInterface{
+public class ProjectManager{
+
+    private static final int nameLength = 16;
+    private static final int descLength = 256;
 
 
-    @Override
+    //@Override
     public boolean processNewProjectRequest(DatabaseObject project) {
         return false;
     }
 
-    @Override
+    //@Override
     public boolean validateUUID(DatabaseObject project) {
         return false;
     }
 
-    @Override
-    public boolean validateName(DatabaseObject project) {
-        return false;
+    //@Override
+    public static boolean validateName(DatabaseObject project) throws Exception {
+        if (project.getName().length() > nameLength){
+            throw new CustomException("Name exceed required amount of characters");
+        } else if (project.getName() == null || project.getName() == ""){
+            throw new CustomException("Cannot have empty name");
+        } else {
+            return true;
+        }
     }
 
-    @Override
-    public boolean validateDescription(DatabaseObject project) {
-        return false;
+    //@Override
+    public boolean validateDescription(DatabaseObject project) throws Exception {
+        if (project.getDescription().length() > descLength){
+            throw new CustomException("Description exceed required amount of characters");
+        } else if (project.getDescription() == null || project.getName().isEmpty()){
+            throw new CustomException("Cannot have empty description");
+        } else {
+            return true;
+        }
     }
 
-    @Override
-    public boolean validateCredentials(DatabaseObject project) {
+    //@Override
+    public boolean validateCredentials(DatabaseObject project) throws Exception {
         return false;
     }
 }
