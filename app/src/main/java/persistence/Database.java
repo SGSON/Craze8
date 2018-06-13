de1f13a97724ba522e25216894f8007618a8fbb3
Buhle Mwanza <buhle.mwanza@gmail.com>
2018-06-12 19:20:01 -0500

    Changed database into signleton class, stores sample data at creation

--- app/src/main/java/persistence/Database.java
+++ app/src/main/java/persistence/Database.java
@@ -4,40 +4,79 @@ import java.util.Hashtable;
 import java.util.UUID;
 
 import domain.DatabaseObject;
+import domain.Project;
 
 
 public class Database implements DatabaseInterface {
 
-    private static Hashtable<UUID, DatabaseObject> DatabaseStorage = new Hashtable<UUID, DatabaseObject>();
+    private static final String[] PROJECT_NAMES = {"InternetFlix", "Sports Analyzer,",
+            "PickMeUp", "Fun Messenger"};
+
+    private static final String[] PROJECT_DESCRIPTIONS = {"View videos online",
+            "Analyze Cool Sports", "Get a ride anywhere", "Talk to your friends"};
+
+    private static final String[] PROJECT_CREDENTIALS = {"C", "C++", "Java", "Python"};
+
+
+    private static Database databaseSingleton;
+    private Hashtable<UUID, DatabaseObject> databaseStorage;
+
+    public static Database getDatabaseInstance() {
+        if(databaseSingleton == null) {
+            databaseSingleton = new Database();
+        }
+
+        return databaseSingleton;
+    }
+
+    /**
+     * Private Constructor Only called if the database has not been
+     * "initialized" Creates and stores sample Projects
+     */
+    private Database() {
+        databaseStorage = new Hashtable<>();
+        storeSampleData();
+    }
+
+    private void storeSampleData() {
+        ArrayList<String> credentials = new ArrayList<String>();
+        for(int i = 0; i < PROJECT_CREDENTIALS.length; i++) {
+            credentials.add(PROJECT_CREDENTIALS[i]);
+        }
+        for(int i = 0; i < PROJECT_NAMES.length; i++) {
+            Project project = new Project(PROJECT_NAMES[i], PROJECT_DESCRIPTIONS[i], credentials);
+            databaseStorage.put(project.getId(), project);
+        }
+    }
 
     @Override
     public void addUser(UUID ID, DatabaseObject user) {
-        DatabaseStorage.put(ID, user);
+        databaseStorage.put(ID, user);
     }
 
     @Override
     public void removeUser(UUID ID) {
-        DatabaseStorage.remove(ID);
+        databaseStorage.remove(ID);
     }
 
     @Override
     public void getUserInfo(UUID ID) {
-        DatabaseStorage.get(ID);
+        databaseStorage.get(ID);
     }
 
     @Override
     public void addProject(UUID ID, DatabaseObject project) {
-        DatabaseStorage.put(ID, project);
+        databaseStorage.put(ID, project);
     }
 
     @Override
     public void removeProject(UUID ID) {
-        DatabaseStorage.remove(ID);
+        databaseStorage.remove(ID);
     }
 
     @Override
     public DatabaseObject getProjectOwner(UUID projectID) {
-        DatabaseStorage.get(projectID);
+        databaseStorage.get(projectID);
         return null;
     }
 
@@ -55,7 +94,6 @@ public class Database implements DatabaseInterface {
 
     @Override
     public ArrayList<DatabaseObject> getProjectSequential() {
-        return new ArrayList<>(DatabaseStorage.values());
+        return null;
     }
-
 }
