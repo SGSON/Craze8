641b76e54bc8c4d775f259736bc306bead78ef18
Levi <leviguo9605@gmail.com>
2018-06-28 15:54:32 -0500

    add aplication folder

---
+++ app/src/main/java/comp3350/ppms/application/Service.java
@@ -0,0 +1,45 @@
+package comp3350.ppms.application;
+
+import comp3350.ppms.persistence.UPDatabaseInterface;
+import comp3350.ppms.persistence.UserDatabaseInterface;
+import comp3350.ppms.persistence.database.ProjectDatabase;
+import comp3350.ppms.persistence.ProjectDatabaseInterface;
+import comp3350.ppms.persistence.database.UPDatabase;
+import comp3350.ppms.persistence.database.UserDatabase;
+
+public class Service {
+
+    private static ProjectDatabaseInterface projectDatabaseInterface = null;
+    private static UserDatabaseInterface userDatabaseInterface = null;
+    private static UPDatabaseInterface upDatabaseInterface = null;
+
+    public static synchronized ProjectDatabaseInterface getProjectDatabaseInterface()
+    {
+        if (projectDatabaseInterface == null)
+        {
+            projectDatabaseInterface = new ProjectDatabase();
+        }
+
+        return projectDatabaseInterface;
+    }
+
+    public static synchronized UserDatabaseInterface getUserDatabaseInterface()
+    {
+        if(userDatabaseInterface == null)
+        {
+            userDatabaseInterface = new UserDatabase();
+        }
+
+        return userDatabaseInterface;
+    }
+
+    public static synchronized UPDatabaseInterface getUPDatabaseInterface()
+    {
+        if(upDatabaseInterface == null)
+        {
+            upDatabaseInterface = new UPDatabase();
+        }
+
+        return upDatabaseInterface;
+    }
+}
