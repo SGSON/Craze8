5dfcf0c6d97c4068ce18bdc99d91ba5a42230674
Adam <frankthetannk@protonmail.com>
2018-07-03 07:24:56 -0500

    Commenting

--- app/src/main/java/comp3350/ppms/presentation/LoginActivity.java
+++ app/src/main/java/comp3350/ppms/presentation/LoginActivity.java
@@ -56,6 +56,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             //Validate account name (will be a valid User or null)
             User validUser = userManager.validateUserName(userNickname);
 
+            //Pass userName to next Activity and start the intent
             if (validUser != null){
                 intent = new Intent(LoginActivity.this, MainActivity.class);
                 intent.putExtra("userName", userNickname);
