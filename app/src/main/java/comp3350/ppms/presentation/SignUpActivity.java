38aa08f97446e4cbcb6664ee8ba34278d4dd9fa8
Buhle Mwanza <buhle.mwanza@gmail.com>
2018-07-03 22:22:14 -0500

    moved DB set up to Log in Activity

--- app/src/main/java/comp3350/ppms/presentation/SignUpActivity.java
+++ app/src/main/java/comp3350/ppms/presentation/SignUpActivity.java
@@ -14,12 +14,6 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
 
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import comp3350.ppms.application.Main;
 import comp3350.ppms.domain.User;
 import comp3350.ppms.logic.UserManager;
 
@@ -44,7 +38,6 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        copyDatabaseToDevice();
         userManager = new UserManager();
 
         userNicknameEdit = (EditText) findViewById(R.id.user_nickname);
@@ -104,56 +97,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         return new User(userNickname, userPassword);
     }
-    private void copyDatabaseToDevice() {
-        final String DB_PATH = "db";
-        String[] assetNames;
-        Context context = getApplicationContext();
-        File dataDirectory = context.getDir(DB_PATH, Context.MODE_PRIVATE);
-        AssetManager assetManager = getAssets();
 
-        try {
-
-            assetNames = assetManager.list(DB_PATH);
-            for (int i = 0; i < assetNames.length; i++) {
-                assetNames[i] = DB_PATH + "/" + assetNames[i];
-            }
-
-            copyAssetsToDirectory(assetNames, dataDirectory);
-
-            Main.setDBPathName(dataDirectory.toString() + "/" + Main.getDBPathName());
-
-        } catch (final IOException ioe) {
-            Messages.warning(this, "Unable to access application data: " + ioe.getMessage());
-        }
-    }
-
-    public void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
-        AssetManager assetManager = getAssets();
-
-        for (String asset : assets) {
-            String[] components = asset.split("/");
-            String copyPath = directory.toString() + "/" + components[components.length - 1];
-
-            char[] buffer = new char[1024];
-            int count;
-
-            File outFile = new File(copyPath);
-
-            if (!outFile.exists()) {
-                InputStreamReader in = new InputStreamReader(assetManager.open(asset));
-                FileWriter out = new FileWriter(outFile);
-
-                count = in.read(buffer);
-                while (count != -1) {
-                    out.write(buffer, 0, count);
-                    count = in.read(buffer);
-                }
-
-                out.close();
-                in.close();
-            }
-        }
-    }
 
     private String validateUserData(User user) {
 
